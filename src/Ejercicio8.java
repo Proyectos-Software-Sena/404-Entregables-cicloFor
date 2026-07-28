aa2fe776d7ed15b09046fafebb2882fe9f1ae3ec
Dylan Perez <xdylanfelipe25@gmail.com>
2026-07-28 17:19:42 -0500

    Ejercicio 8: Generador de Códigos (Inversor de Cadenas)

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,7 +1,16 @@
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Ejercicio8 {
-    public static void main (String[] args) {
-        //Ingrese su código
+    public static void main(String[] args){
+        // Generador de Códigos (Inversor de Cadenas)
+        String palabra ;
+        String invertido = "";
+        palabra = JOptionPane.showInputDialog("ingrese  su palabra clave ");
+        for (int i = palabra.length()-1; i >= 0; i--){
+            invertido = invertido + palabra.charAt(i);
+        }
+        //fin
+        JOptionPane.showMessageDialog(null,"clave invertida:"+invertido);
     }
 }
+
