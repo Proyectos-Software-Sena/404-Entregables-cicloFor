3488317083ed53513f0391ed2786a674a11e47a3
Dylan Perez <xdylanfelipe25@gmail.com>
2026-07-28 14:59:06 -0500

    Ejercicio 5:  Proyección de Ahorros Programados

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 public class Ejercicio5 {
-    public static void main (String[] args) {
-        //Ingrese su código
+    public static void main(String[] args){
+        double ahorromensual;
+        int cantidaddemeses;
+        double acumulado = 0;
+        ahorromensual = Double.parseDouble(JOptionPane.showInputDialog("ingrese el ahorro mensual:")
+        );
+        cantidaddemeses = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de meses:")
+        );
+        //proceso
+        for(int i = 1; i <=cantidaddemeses; i++) {
+            acumulado += ahorromensual;
+            //fin
+            JOptionPane.showMessageDialog(null, "Mes" + i + ": $" + acumulado);
+        }
     }
 }
