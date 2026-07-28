dc9bf7a26f1e0a2578bea2b731d48d055169931b
Dylan Perez <xdylanfelipe25@gmail.com>
2026-07-28 17:22:28 -0500

    Ejercicio 9: Modelo de Crecimiento Bacteriano

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Ejercicio9 {
-    public static void main (String[] args) {
-        //Ingrese su código
+    public  static void main(String[] args){
+        int bacterias;
+        int horas;
+        String resuelto = "";
+        bacterias = Integer.parseInt((JOptionPane.showInputDialog("ingrese la cantidad inicial de las bacterias "))
+        );
+        horas = Integer.parseInt(JOptionPane.showInputDialog("ingrese la cantidad de horas ")
+        );
+        for (int i =1 ; i <= horas; i++ ){
+            bacterias =  bacterias * 2;
+            resuelto = resuelto + "horas " + i + " : " + bacterias + "bacterias\n " ;
+        }
+        JOptionPane.showMessageDialog(null,resuelto);
     }
 }
