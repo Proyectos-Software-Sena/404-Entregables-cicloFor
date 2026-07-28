381ef8bd57e1ab42a4f0b4ea709ea5a105f6959b
Dylan Perez <xdylanfelipe25@gmail.com>
2026-07-28 14:30:01 -0500

    Ejercicio 1:Cierre de Caja con for

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class Ejercicio1 {
-    public static void main(String[] args) {
-        //Ingrese su código
+    public  static void main(String[] args){
+        //Cierre de Caja (Totalizar Ventas
+        int ventas ;
+        int valorventa;
+        int total = 0;
+        ventas = Integer.parseInt(JOptionPane.showInputDialog("¿cuantas ventas se realizaron en el dia? ")
+        );
+        for (int i  = 1; i <= ventas; i++){
+            valorventa =  Integer.parseInt(JOptionPane.showInputDialog("el valor de cada una de las ventas:" + i)
+
+            );
+            total = total + valorventa;
+        }
+        JOptionPane.showMessageDialog(null,"El total de ventas del día es: " + total);
     }
+
 }
