eaeb7b6ca6a0fa94f67804407f42baef887513b5
Dylan Perez <xdylanfelipe25@gmail.com>
2026-07-28 17:15:53 -0500

    Ejercicio 7: Tabla de Conversión de Divisas

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+
 public class Ejercicio7 {
-    public static void main(String[] args) {
-        //Ingrese su código
+    public static void main(String[] args){
+//Tabla de Conversión de Divisas
+        double  tasa ;
+        double  conversion;
+        String tabla = "";
+        tasa = Double.parseDouble(JOptionPane.showInputDialog("ingrese  la tasa  de cambio de cop a dolar")
+        );
+        for (int dolar = 1; dolar <= 10; dolar++){
+            conversion = dolar * tasa;
+            tabla = tabla + dolar +"usd =" +conversion + "cop\n";
+        }
+        //fin
+        JOptionPane.showMessageDialog(null,tabla);
+
+
     }
 }
+
