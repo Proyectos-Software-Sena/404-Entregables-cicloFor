1c7fc6a92aac6cf44610b99d97e3d04119eb0941
valentina-vanegas <vanegashayav@gmail.com>
2026-07-28 17:20:16 -0500

    ejercicio 10: serie de fibonacci

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 public class Ejercicio10 {
     public static void main(String[] args) {
+
         int n = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de términos"));
 
         int a = 0;
