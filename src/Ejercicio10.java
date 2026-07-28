4b85393935d87882e0a2ea9201b3dbfd389dc662
michelle-suarez <ms2890503@gmail.com>
2026-07-28 17:17:02 -0500

    ejercicio 10: estimacion agil.

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Ejercicio10 {
     public static void main(String[] args) {
         //Ingrese su código
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Cantidad de terminos:");
