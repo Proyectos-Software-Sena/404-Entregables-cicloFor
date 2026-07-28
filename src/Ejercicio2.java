6a047ac998b269ba789a7b73be1e97e6dcfd32f6
jeronimo buitrago <jeronimobuitragogarcia7@gmail.com>
2026-07-28 14:24:14 -0500

    Ejercicio 1: cierre de caja

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,5 +1,30 @@
+import javax.swing.*;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int cantidadPiezas;
+        int aprobadas = 0;
+        int defectuosas = 0;
+        double peso;
+
+        cantidadPiezas = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas piezas se van a revisar?"));
+
+        for (int i = 1; i <= cantidadPiezas; i++) {
+
+            peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso de la pieza " + i + " (g):")
+            );
+
+            if (peso < 100 || peso > 120) {
+
+                defectuosas++;
+
+            } else {
+
+                aprobadas++;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null,"Piezas aprobadas: " + aprobadas + "\nPiezas defectuosas: " + defectuosas);
     }
 }
