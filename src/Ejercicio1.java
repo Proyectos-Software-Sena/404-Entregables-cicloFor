87d5fcfdbaa00821b20b19b560c5614ba9fd190f
Juan Diego <juandiegomejiacardona2021@gmail.com>
2026-07-28 14:08:20 -0500

    Ejercicio 1: ciclo For

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,17 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        //Ingrese su código
+            Scanner tc = new Scanner(System.in);
+            System.out.println("Hola, bienvenido");
+            System.out.println("--------------------");
+            System.out.println("Ingresa la cantidad de ventas realizadas en el dia de hoy");
+            int ventas = tc.nextInt();
+            double ingresos = 0;
+
+            for (int i = 0; i<ventas;i++){
+                System.out.println("Ingresa el valor de la venta "+(i+1));
+                double valor = tc.nextDouble();
+                ingresos+=valor;
+            }
+            System.out.println("El total de ventas en el día es de "+ingresos);
     }
 }
