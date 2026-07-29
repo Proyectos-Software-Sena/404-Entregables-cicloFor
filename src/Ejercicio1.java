481b122ea6aea757f2750abd9aed407e84226e20
Dariangel Esther Barrientos Soto <darie449@gmail.com>
2026-07-28 19:45:58 -0500

    Ejercicio 1: cierre de caja

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,21 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        //Ingrese su código
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de ventas realizadas: ");
+        int ventas = sc.nextInt();
+
+        int total = 0;
+
+        for (int i = 1; i <= ventas; i++) {
+            System.out.print("Ingrese el valor de la venta " + i + ": ");
+            int valor = sc.nextInt();
+
+            total += valor;
+        }
+
+        System.out.println("El total de ventas del día es: $" + total);
+
+        sc.close();
     }
 }
