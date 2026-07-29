d9f553f760524802776875a189e0de66dcbab571
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:49:54 -0500

    ejercicio 9: se soluciono aplicando logica de ejercicios anteriores haciendo mas facil el proceso

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+
 public class Ejercicio9 {
     public static void main (String[] args) {
-        //Ingrese su código
+
+        int bacterias = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad inicial de bacterias"));
+        int horas;
+        int totalBacterias;
+
+
+        horas = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas horas desea proyectar?"));
+
+        totalBacterias = bacterias;
+
+        for (int i = 1; i <= horas; i++) {
+
+            totalBacterias *= 2;
+
+            JOptionPane.showMessageDialog(null, "Hora " + i + ": " + totalBacterias + " bacterias");
+
+        }
     }
 }
