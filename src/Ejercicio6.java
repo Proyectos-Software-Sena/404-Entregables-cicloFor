1bb03cd74841ec3cf96fdf1fb671b822a57a5da4
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:41:00 -0500

    ejercicio 6: se intento algo nuevo para los espaciados del codigo

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,43 @@
+import javax.swing.*;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int cantidadAsistentes;
+        int edad;
+        int menores = 0;
+        int adultos = 0;
+
+        cantidadAsistentes = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "¿Cuántos asistentes hay?"
+                )
+        );
+
+        for (int i = 1; i <= cantidadAsistentes; i++) {
+
+            edad = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            "Ingrese la edad del asistente " + i
+                    )
+            );
+
+            if (edad < 18) {
+
+                menores++;
+
+            } else {
+
+                adultos++;
+
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Menores: " + menores +
+                        "\nAdultos: " + adultos
+        );
     }
 }
