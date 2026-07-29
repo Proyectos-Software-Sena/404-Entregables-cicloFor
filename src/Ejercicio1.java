787215dfc3c63032f58468c1d74e22f665b2db44
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 20:51:55 -0500

    se actualizo la vista del codigo, cambiando espaciados y palabras para las variables

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -4,15 +4,15 @@ public class Ejercicio1 {
     public static void main(String[] args) {
 
         int cantidadVentas= Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas ventas se realizaron hoy?"));
-        int totalVentas = 0;
+        int total = 0;
 
         for (int i = 1;i <= cantidadVentas; i++) {
             int valorVenta = Integer.parseInt(
                     JOptionPane.showInputDialog("Ingrese el valor de la venta " + i));
 
-            totalVentas += valorVenta;
+            total += valorVenta;
 
         }
-        JOptionPane.showMessageDialog(null, "El total de ventas del día es: $" + totalVentas);
+        JOptionPane.showMessageDialog(null, "El total de ventas del día es: $" + total);
     }
 }
