1a8935e6656a1e44fecf34a992d7b1f245bc8759
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:02:51 -0500

    Ejercicio 3: se utilizo varias formas para nombrar las variables para que sean coherentes y mejor ordenadas

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int lecturas= Integer.parseInt(JOptionPane.showInputDialog("ingresar cantidad de temperaturas a registrar "));
+        int temperaturas;
+        int total=0;
+
+        for (int i= 1; i <= lecturas; i++){
+
+        }
+
+
     }
 }
