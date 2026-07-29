444007a741e272b561aef72487c11ef8c97b8497
Dariangel Esther Barrientos Soto <darie449@gmail.com>
2026-07-28 20:37:54 -0500

    Ejercicio6:clasificacion de edades

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,5 +1,29 @@
 public class Ejercicio6 {
     public static void main(String[] args) {
         //Ingrese su código
+        Scanner sc = new Scanner(System.in);
+
+        int asistentes;
+        int edad;
+        int menores = 0;
+        int adultos = 0;
+
+        System.out.print("Cantidad de asistentes: ");
+        asistentes = sc.nextInt();
+
+        for (int i = 1; i <= asistentes; i++) {
+            System.out.print("Edad del asistente " + i + ": ");
+            edad = sc.nextInt();
+
+            if (edad < 18) {
+                menores++;
+            } else {
+                adultos++;
+            }
+        }
+
+        System.out.println("Menores: " + menores);
+        System.out.println("Adultos: " + adultos);
+
     }
 }
