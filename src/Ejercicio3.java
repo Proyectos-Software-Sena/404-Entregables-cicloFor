5fe11afa9ebd1879422889802597b655f81358d8
Dariangel Esther Barrientos Soto <darie449@gmail.com>
2026-07-28 20:12:07 -0500

    Ejercicio3: cambio de temperatura

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,5 +1,25 @@
 public class Ejercicio3 {
     public static void main(String[] args) {
-        //Ingrese su código
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Cantidad de lecturas: ");
+        int lecturas = sc.nextInt();
+
+        double maxima = -999;
+
+        for(int i = 1; i <= lecturas; i++){
+
+            System.out.print("Temperatura " + i + ": ");
+            double temperatura = sc.nextDouble();
+
+            if(temperatura > maxima){
+                maxima = temperatura;
+            }
+        }
+
+        System.out.println("La temperatura máxima registrada fue: "
+                + maxima + "°C");
+
+        sc.close();
     }
 }
