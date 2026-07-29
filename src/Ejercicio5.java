8eedbc9a5afcb9190497b52148ed21e32f417b49
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:38:13 -0500

    ejercicio 5: en este no hubo tantas complicaciones, por metodos utilizados anteriormente y repasos anteriores con el while

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+
 public class Ejercicio5 {
     public static void main (String[] args) {
-        //Ingrese su código
+
+        int  meses = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos meses desea proyectar?"));
+        int ahorroMensual = Integer.parseInt(JOptionPane.showInputDialog("¿Cuánto desea ahorrar cada mes?"));
+        int ahorroAcumulado = 0;
+
+
+        for (int i = 1; i <= meses; i++) {
+
+            ahorroAcumulado += ahorroMensual;
+
+            JOptionPane.showMessageDialog(null,
+                    "Mes " + i + ": $" + ahorroAcumulado
+            );
+
+        }
     }
 }
