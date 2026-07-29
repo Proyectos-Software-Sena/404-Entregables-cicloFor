1a2d2c33a82892ee165ba3cd4c26a637e936a112
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:44:30 -0500

    ejercicio 7: igual que algunos anteriores no hubo tanto problema en su solucion

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 public class Ejercicio7 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int trm;
+        int pesos;
+
+        trm = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Ingrese la TRM del día"
+                )
+        );
+
+        for (int i = 1; i <= 10; i++) {
+
+            pesos = i * trm;
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    i + " USD = " + pesos + " COP"
+            );
+
+        }
     }
 }
