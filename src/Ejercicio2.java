a0282c9f9df38b9843e70392cc2403316579d32f
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 22:47:49 -0500

    se hizo cambio en los condicionales mas que todo del IF ya que debe ir dentro de las llaves del FOR

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -4,13 +4,25 @@ public class Ejercicio2 {
     public static void main(String[] args) {
 
         int npiezas= Integer.parseInt(JOptionPane.showInputDialog("Cuantas piezas se van a revisar: "));
-        int peso= Integer.parseInt(JOptionPane.showInputDialog("Ingresar peso de la pieza "));
         int aprobadas= 0;
         int defectuosas= 0;
 
         for (int i= 1; i <= npiezas; i++){
 
+            int peso= Integer.parseInt(JOptionPane.showInputDialog("Ingresar peso de la pieza " + i));
+
+            if (peso < 100 || peso > 120){
+
+                defectuosas++;
+
+            }else {
+
+                aprobadas++;
+            }
+
         }
+
+        JOptionPane.showMessageDialog(null,"piezas aprobadas: " + aprobadas + " piezas defectuosas: " + defectuosas);
 
 
     }
