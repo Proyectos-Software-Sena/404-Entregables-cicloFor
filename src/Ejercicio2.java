5c561e422579eb6b5cf75e5678e42d25edb2f456
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 22:19:49 -0500

    Ejercicio 2: Se hizo en lo posible la reduccion de contenido de algunas variables

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int npiezas= Integer.parseInt(JOptionPane.showInputDialog("Cuantas piezas se van a revisar: "));
+        int peso= Integer.parseInt(JOptionPane.showInputDialog("Ingresar peso de la pieza "));
+
+
     }
 }
