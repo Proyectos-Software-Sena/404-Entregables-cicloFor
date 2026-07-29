936bb586a2a3725449d7aaab90907a024c99e804
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:46:27 -0500

    ejercicio 8: se presento problema en las comparaciones dentro del ciclo for, las cuales se pudieron solucionar

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,7 +1,25 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Ejercicio8 {
     public static void main (String[] args) {
-        //Ingrese su código
+
+        String palabra;
+        String palabraInvertida = "";
+
+        palabra = JOptionPane.showInputDialog(
+                "Ingrese una palabra"
+        );
+
+        for (int i = palabra.length() - 1; i >= 0; i--) {
+
+            palabraInvertida += palabra.charAt(i);
+
+        }
+
+        JOptionPane.showMessageDialog(
+                null,
+                "Cadena invertida: " + palabraInvertida
+        );
     }
 }
