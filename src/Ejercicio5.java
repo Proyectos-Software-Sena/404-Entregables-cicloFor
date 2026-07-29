99bca19e35206a33d603b851f2e8f380afeda4ec
Dariangel Esther Barrientos Soto <darie449@gmail.com>
2026-07-28 20:34:43 -0500

    Ejercicio5: proyeccion de ahorros

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -1,5 +1,23 @@
 public class Ejercicio5 {
     public static void main (String[] args) {
-        //Ingrese su código
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Monto mensual a ahorrar: ");
+        int ahorro = sc.nextInt();
+
+        System.out.print("Cantidad de meses: ");
+        int meses = sc.nextInt();
+
+        int acumulado = 0;
+
+        for(int i = 1; i <= meses; i++){
+
+            acumulado += ahorro;
+
+            System.out.println("Mes " + i + ": $" + acumulado);
+        }
+
+        sc.close();
+
     }
 }
