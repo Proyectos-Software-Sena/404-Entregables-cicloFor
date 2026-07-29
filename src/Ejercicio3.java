b281cf470890bf11330deebfb6d5d0e598d016e3
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:24:42 -0500

    se soluciono el problema de los condicionales los cual causaban que no funcionara el For

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -4,13 +4,26 @@ public class Ejercicio3 {
     public static void main(String[] args) {
 
         int lecturas= Integer.parseInt(JOptionPane.showInputDialog("ingresar cantidad de temperaturas a registrar "));
-        int temperaturas;
         int total=0;
 
         for (int i= 1; i <= lecturas; i++){
 
-        }
+            int temperaturas= Integer.parseInt(JOptionPane.showInputDialog("ingresar temperatura " + i));
 
+            if (i == 1){
+
+                total = temperaturas;
+
+            }else {
+
+                if (temperaturas > total){
+
+                    total = temperaturas;
+                }
+            }
+
+        }
+        JOptionPane.showMessageDialog(null, "La temperatura máxima registrada fue: " + total + "°C");
 
     }
 }
