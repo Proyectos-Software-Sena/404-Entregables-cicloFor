17435924f004b03251a7a372fc8f3bd2fe5e9d94
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 22:23:50 -0500

    se produjo un error con el contador al iniciar el programa y por ende no se detenia el bucle

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -5,6 +5,12 @@ public class Ejercicio2 {
 
         int npiezas= Integer.parseInt(JOptionPane.showInputDialog("Cuantas piezas se van a revisar: "));
         int peso= Integer.parseInt(JOptionPane.showInputDialog("Ingresar peso de la pieza "));
+        int aprobadas= 0;
+        int defectuosas= 0;
+
+        for (int i= 1; i <= npiezas; i++){
+
+        }
 
 
     }
