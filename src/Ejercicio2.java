21a4f9b5355747be164c5d1f1b4942881010a20c
Dariangel Esther Barrientos Soto <darie449@gmail.com>
2026-07-28 20:07:01 -0500

    Ejercicio2: control de calidad

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,5 +1,25 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-        //Ingrese su código
+        Scanner sc = new Scanner(System.in);
+
+        int piezas, aprobadas = 0, defectuosas = 0;
+        double peso;
+
+        System.out.print("Cantidad de piezas a revisar: ");
+        piezas = sc.nextInt();
+
+        for (int i = 1; i <= piezas; i++) {
+            System.out.print("Peso de la pieza " + i + ": ");
+            peso = sc.nextDouble();
+
+            if (peso < 100 || peso > 120) {
+                defectuosas++;
+            } else {
+                aprobadas++;
+            }
+        }
+
+        System.out.println("Piezas aprobadas: " + aprobadas);
+        System.out.println("Piezas defectuosas: " + defectuosas);
     }
 }
