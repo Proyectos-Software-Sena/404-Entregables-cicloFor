49647eda3fb749cff5532e6b31417305d9a0e70e
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 20:25:54 -0500

    Ejercicio 1: hice cambios en el ciclo, de variables y de contador

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Ejercicio1 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+        int cantidadVentas= Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas ventas se realizaron hoy?"));
+        int totalVentas = 0;
+
+        for (int i = 1;i <= cantidadVentas; i++) {
+            int valorVenta = Integer.parseInt(
+                    JOptionPane.showInputDialog("Ingrese el valor de la venta " + i));
+
+            totalVentas += valorVenta;
+
+        }
+        JOptionPane.showMessageDialog(null, "El total de ventas del día es: $" + totalVentas);
     }
 }
