413c31bf7879b2fa04dc256b6d2bdfea2b605194
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-07-28 23:52:59 -0500

    ejercicio 10: problema con el orden de las ecuaciones en la sucecion del for

--- src/Ejercicio10.java
+++ src/Ejercicio10.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+
 public class Ejercicio10 {
     public static void main(String[] args) {
-        //Ingrese su código
+
+
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos términos desea generar?"));
+        int primero = 0;
+        int segundo = 1;
+        int siguiente;
+
+        String serie = "";
+
+
+        for (int i = 1; i <= cantidad; i++) {
+
+            serie += primero + " ";
+
+            siguiente = primero + segundo;
+
+            primero = segundo;
+
+            segundo = siguiente;
+
+        }
+
+        JOptionPane.showMessageDialog(null, serie);
     }
 }
